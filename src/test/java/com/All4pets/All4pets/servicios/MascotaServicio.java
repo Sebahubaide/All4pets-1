53f8dbe3bf5c16c47a5edff62b8a8960c1c49098
Sebahubaide <shubaide@gmail.com>
2022-05-16 20:31:10 -0300

    Create MascotaServicio.java

---
+++ src/test/java/com/All4pets/All4pets/servicios/MascotaServicio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.All4pets.All4pets.servicios;
+
+/**
+ *
+ * @author Sebastian Hubaide
+ */
+public class MascotaServicio {
+    
+}
